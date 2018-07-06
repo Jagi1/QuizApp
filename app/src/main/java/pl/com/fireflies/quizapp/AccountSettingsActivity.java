c085d2bb8d5401e477ef83072bed4792e23957a7
DanBishopPL <daniel.biskup@outlook.com>
2018-07-06 09:45:20 +0200

    Dodawanie zdjecia do quizu.

--- app/src/main/java/pl/com/fireflies/quizapp/AccountSettingsActivity.java
+++ app/src/main/java/pl/com/fireflies/quizapp/AccountSettingsActivity.java
@@ -2,19 +2,15 @@ package pl.com.fireflies.quizapp;
 
 import android.Manifest;
 import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,11 +25,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-import com.squareup.picasso.Picasso;
-import com.squareup.picasso.Target;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class AccountSettingsActivity extends AppCompatActivity implements View.OnClickListener {
@@ -165,49 +157,6 @@ public class AccountSettingsActivity extends AppCompatActivity implements View.O
                         }
                     });
         }
-    }
-
-    private Target picassoImageTarget(Context context, final String imageDir, final String imageName) {
-        Log.d("picassoImageTarget", " picassoImageTarget");
-        ContextWrapper cw = new ContextWrapper(context);
-        final File directory = cw.getDir(imageDir, Context.MODE_PRIVATE); // path to /data/data/yourapp/app_imageDir
-        return new Target() {
-            @Override
-            public void onBitmapLoaded(final Bitmap bitmap, Picasso.LoadedFrom from) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        final File myImageFile = new File(directory, imageName); // Create image file
-                        FileOutputStream fos = null;
-                        try {
-                            fos = new FileOutputStream(myImageFile);
-                            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        } finally {
-                            try {
-                                fos.close();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                        Log.i("image", "image saved to >>>" + myImageFile.getAbsolutePath());
-
-                    }
-                }).start();
-            }
-
-            @Override
-            public void onBitmapFailed(Exception e, Drawable errorDrawable) {
-
-            }
-
-            @Override
-            public void onPrepareLoad(Drawable placeHolderDrawable) {
-                if (placeHolderDrawable != null) {
-                }
-            }
-        };
     }
 
     protected void initViews() {
--- app/src/main/java/pl/com/fireflies/quizapp/NewQuizActivity.java
+++ app/src/main/java/pl/com/fireflies/quizapp/NewQuizActivity.java
@@ -1,24 +1,43 @@
 package pl.com.fireflies.quizapp;
 
+import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
 
 public class NewQuizActivity extends AppCompatActivity implements View.OnClickListener {
     private Intent intent;
     private Toolbar toolbar;
     private ImageButton avatar, settings;
-    private Button addQuizButton;
+    private ImageView image1View;
+    private Button addQuizButton, clearFormButton, chooseImage1;
     private EditText namequiz, categoryquiz, questionquiz1, okodp1, otherodp1, questionquiz2, okodp2, otherodp2,
             questionquiz3, okodp3, otherodp3, questionquiz4, okodp4, otherodp4, questionquiz5, okodp5, otherodp5;
+    private Uri uriFilePath;
+    private String image1URL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,15 +65,89 @@ public class NewQuizActivity extends AppCompatActivity implements View.OnClickLi
                 intent = new Intent(NewQuizActivity.this, AccountSettingsActivity.class);
                 NewQuizActivity.this.startActivity(intent);
                 break;
+            case R.id.choose_image1:
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    DataHolder.requestStoragePermission(this);
+                } else {
+                    chooseImage();
+                }
+                break;
+            case R.id.clear_form:
+                clearForm();
+                Toast.makeText(getApplicationContext(),"Wyczyszczono formularz!", Toast.LENGTH_LONG).show();
+                break;
             case R.id.add_quiz:
+                uploadImage();
                 addQuiz();
+                Toast.makeText(getApplicationContext(),"Pomyslnie dodano Quiz!", Toast.LENGTH_LONG).show();
                 break;
         }
     }
 
-    public void addQuiz() {
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), DataHolder.PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == DataHolder.PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            uriFilePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uriFilePath);
+                image1View.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void uploadImage() {
+        if (uriFilePath != null) {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            final StorageReference refStoragePath = DataHolder.getInstance().storageReference
+                    .child("images").child(uriFilePath.getLastPathSegment());
+
+            image1URL = refStoragePath.toString();
+            UploadTask uploadTask = refStoragePath.putFile(uriFilePath);
+            uploadTask
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(NewQuizActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(NewQuizActivity.this, "Failed " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded " + (int) progress + "%");
+                        }
+                    });
+        }
+    }
+
+    private void addQuiz() {
         DatabaseReference pathName = DataHolder.firebaseDatabase.child("quizy").child(categoryquiz.getText().toString()).child(namequiz.getText().toString());
 
+        // image1URL zawiera sciezke URL obrazka (Storage > images > obrazek)
+        pathName.child(questionquiz1.getText().toString()).child("image1").setValue(image1URL);
         pathName.child(questionquiz1.getText().toString()).child("okodp").setValue(okodp1.getText().toString());
         pathName.child(questionquiz1.getText().toString()).child("otherodp").setValue(otherodp1.getText().toString());
 
@@ -69,12 +162,16 @@ public class NewQuizActivity extends AppCompatActivity implements View.OnClickLi
 
         pathName.child(questionquiz5.getText().toString()).child("okodp").setValue(okodp5.getText().toString());
         pathName.child(questionquiz5.getText().toString()).child("otherodp").setValue(otherodp5.getText().toString());
-        Toast.makeText(getApplicationContext(),"Pomyslnie dodano Quiz!", Toast.LENGTH_LONG).show();
+    }
+
+    private void clearForm() {
         namequiz.getText().clear();
         categoryquiz.getText().clear();
+
         questionquiz1.getText().clear();
         okodp1.getText().clear();
         otherodp1.getText().clear();
+
         questionquiz2.getText().clear();
         okodp2.getText().clear();
         otherodp2.getText().clear();
@@ -88,15 +185,21 @@ public class NewQuizActivity extends AppCompatActivity implements View.OnClickLi
         okodp5.getText().clear();
         otherodp5.getText().clear();
     }
+
     protected void initViews() {
         toolbar = (Toolbar) findViewById(R.id.user_bar);
         avatar = (ImageButton) findViewById(R.id.avatar);
         settings = (ImageButton) findViewById(R.id.settings_gear);
         addQuizButton = (Button) findViewById(R.id.add_quiz);
+        clearFormButton = (Button) findViewById(R.id.clear_form);
+        chooseImage1 = (Button) findViewById(R.id.choose_image1);
+        image1View = (ImageView) findViewById(R.id.image1);
         avatar.setImageBitmap(DataHolder.getInstance().avatarBitmap);
         avatar.setOnClickListener(this);
         settings.setOnClickListener(this);
         addQuizButton.setOnClickListener(this);
+        clearFormButton.setOnClickListener(this);
+        chooseImage1.setOnClickListener(this);
 
         namequiz = (EditText) findViewById(R.id.nameQuiz);
         categoryquiz = (EditText) findViewById(R.id.categoryQuiz);
